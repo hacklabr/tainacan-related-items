c1c37e475a56a46ffd56deae37ee11fdffe6f250
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-06-08 15:07:32 -0300

    initial commit

---
+++ admin-page.php
@@ -0,0 +1,155 @@
+<style>
+	.tainacan-related-items .error {
+		border-left: 5px solid red;
+		margin-right: 10px;
+	}
+	.tainacan-related-items .warning {
+		border-left: 5px solid orange;
+		margin-right: 10px;
+	}
+	.tainacan-related-items .good {
+		border-left: 5px solid green;
+		margin-right: 10px;
+	}
+	.tainacan-related-items .impartial {
+		border-left: 5px solid gray;
+		margin-right: 10px;
+	}
+
+    .tainacan-related-items th, .tainacan-related-items td {
+        padding: 1px;
+        width: auto !important;
+    }
+
+    .tainacan-related-items .collection {
+        border:1px solid #ddd;
+        border-right: 0;
+        border-bottom: 0;
+        padding: 0.5em 1em;
+    }
+
+    .tainacan-related-items .collection .collection-options {
+        padding: 0 1em;
+    }
+
+    .tainacan-related-items table {
+        width: auto;
+    }
+
+
+    .tainacan-related-items h3 {
+        padding:0;
+        margin:0;
+    }
+
+    .tainacan-related-items thead td {
+        padding: 1em 1px;
+    }
+</style>
+
+<script>
+(($) => {
+    $(() => {
+        function setContainerStatus(checkbox){
+            let $container = $('#options-' + $(checkbox).data('collectionContainer'));
+            console.log('#' + $(checkbox).data('collectionContainer'), $container)
+            if($(checkbox).is(':checked')){
+                $container.show();
+            } else {
+                $container.hide();
+            }
+        }
+        $('.js--collection-checkbox').each(function() {
+            setContainerStatus(this);
+        });
+
+        $('.js--collection-checkbox').click(function(){
+            setContainerStatus(this);
+        });
+    });
+})(jQuery);
+</script>
+
+<?php 
+settings_errors('tainacan_related_items_messages'); 
+
+$collections = $this->get_collections();
+$collection_names = array_map(function ($coll) { return $coll->get_db_identifier(); }, $collections);
+
+?>
+<div class="wrap tainacan-related-items">
+    <h1><?php _e('Tainacan Related Items Configuration', 'tainacan-related-items'); ?></h1>
+    <form action="options.php" method="post">
+        <?php settings_fields('tainacan_related_items'); ?>
+        <?php 
+        foreach($collections as $collection): 
+            $collection_id = $collection->get_db_identifier();
+            $taxonomies = $this->get_collection_taxonomies($collection);
+            $enabled_collections = $options ? $options[$collection_id]['collections'] : $collection_names;
+            ?>
+            <section class="collection">
+                <h2>
+                    <label>
+                        <input type="checkbox" 
+                                name="tainacan_related_items[<?= $collection_id ?>][uses]" 
+                                class="js--collection-checkbox"
+                                <?php checked('on', @$options[$collection_id]['uses']) ?>
+                                data-collection-container="<?= $collection_id ?>" >
+
+                        <?= $collection->get_name() ?>
+                    </label>
+                </h2>
+                
+                <section id="options-<?= $collection_id ?>" class="js--collection-options collection-options">
+                    
+                    <h4><?php _e('Select the collections from which you want to get the items', 'tainacan-related-items') ?>:</h4>
+                    <p>
+                        <?php 
+                        
+                        foreach($collections as $col):
+
+                            ?>
+                            <label>
+                                <input type="checkbox" 
+                                        name="tainacan_related_items[<?= $collection_id ?>][collections][]" 
+                                        value="<?= $col->get_db_identifier() ?>"
+                                        <?php checked(true, in_array($col->get_db_identifier(), $enabled_collections)) ?> >
+                                
+                                <?= $col->get_name() ?>
+                            </label>
+                        <?php endforeach ?>
+                    </p>
+                    
+                    <?php if($taxonomies): ?>
+
+                        <h4><?php _e('Taxonomies weight', 'tainacan-related-items') ?></h4>
+                        <p>
+                            <?php _e('Leave the field blank to do not consider the taxonomy for the relationship', 'tainacan-related-items') ?>
+                        </p>
+
+                        <table class="form-table">
+                            <tbody>
+                                <?php foreach($taxonomies as $taxonomy): ?>
+                                <tr>
+                                    <th scope="row"><?= $taxonomy->get_name() ?></th>
+                                    <td>
+                                        <input 
+                                            type="number" 
+                                            step="0.1" 
+                                            name="tainacan_related_items[<?= $collection_id ?>][weights][<?= $taxonomy->get_db_identifier() ?>]"
+                                            value="<?= @$options[$collection_id]['weights'][$taxonomy->get_db_identifier()] ?>"
+                                        >
+                                    </td>
+                                </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    <?php else: ?>
+                        <p><?php _e("This collection does not use any taxonomy", 'tainacan-related-items'); ?>
+                    <?php endif; ?>
+                </section>
+            </section>
+        <?php endforeach; ?>
+        <?php submit_button('Save Settings'); ?>
+    </form>
+</div>
---
+++ tainacan-related-items.php
@@ -0,0 +1,275 @@
+<?php
+/*
+Plugin Name: Tainacan Related Items
+Description: 
+Version:     1.0.0
+Author:      hacklab/
+Author URI:  https://hacklab.com.br/
+License:     GPL2
+*/
+
+class TainacanRelatedItems
+{
+
+    protected static $instance = null;
+
+    public function __construct()
+    {
+        self::$instance = $this;
+        add_action('admin_menu', [$this, 'register_admin_page'], 100);
+        add_action('admin_menu', [$this, 'register_admin_page_settings'], 100);
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    function register_admin_page()
+    {
+        add_submenu_page(
+            'tainacan_admin',
+            __('Related Items', 'tainacan-related-items'),
+            __('Related Items', 'tainacan-related-items'),
+            'manage_options',
+            'tainacan_related_items',
+            [$this, 'settings_page']
+        );
+    }
+
+    public function register_admin_page_settings()
+    {
+        register_setting('tainacan_related_items', 'tainacan_related_items');
+    }
+
+    public function settings_page()
+    {
+        if (!current_user_can('manage_options')) {
+            return;
+        }
+        if (isset($_GET['settings-updated'])) {
+            add_settings_error('tainacan_related_items_messages', 'tainacan_related_items_message', __('Settings Saved', 'tainacan_related_items'), 'updated');
+        }
+
+        $options = get_option('tainacan_related_items', [
+            'collections' => []
+        ]);
+
+        require 'admin-page.php';
+    }
+
+    /**
+     * Returns Tainacan Collections
+     *
+     * @return Tainacan\Entities\Collection[]
+     */
+    public function get_collections()
+    {
+        $repository = \Tainacan\Repositories\Collections::get_instance();
+        return $repository->fetch([], OBJECT);
+    }
+
+    /**
+     * Returns Collection Taxonomies
+     *
+     * @param Tainacan\Entities\Collection $collection
+     * @return Tainacan\Entities\Taxonomy[]
+     */
+    public function get_collection_taxonomies(Tainacan\Entities\Collection $collection)
+    {
+        $metadata = $collection->get_metadata();
+
+        $repository = \Tainacan\Repositories\Taxonomies::get_instance();
+
+        $metadata = array_filter($metadata, function ($item) {
+            return $item->get_metadata_type() === 'Tainacan\Metadata_Types\Taxonomy' ?
+                $item : null;
+        });
+
+        $taxonomies = array_map(function ($metadatum) use ($repository) {
+            $options = $metadatum->get_metadata_type_options();
+            $taxonomy_id = $options['taxonomy_id'];
+
+            return $repository->fetch($taxonomy_id);
+        }, $metadata);
+
+        return $taxonomies;
+    }
+
+    public function get_post_taxonomy_terms($post_id, $taxonomy)
+    {
+
+        $_terms = get_the_terms($post_id, $taxonomy) ?: [];
+
+        $_terms = array_map(function ($el) {
+            return $el->term_taxonomy_id;
+        }, $_terms);
+
+        $result = implode(',', $_terms);
+        return $result ?: -1;
+    }
+
+    /**
+     * Return the configuration for the collection
+     *
+     * @param item $collection_post_type
+     * @return void
+     */
+    public function get_config($collection_post_type)
+    {
+        $config = get_option('tainacan_related_items');
+
+        return isset($config[$collection_post_type]) ? $config[$collection_post_type] : [];
+    }
+
+    /**
+     * Return the rating
+     *
+     * @param [type] $item_id
+     * @param [type] $taxonomy
+     * @param [type] $term_weight
+     * @return void
+     */
+    public function get_taxonomy_rating_sql($item_id, $taxonomy, $term_weight)
+    {
+        global $wpdb;
+
+        $terms = $this->get_post_taxonomy_terms($item_id, $taxonomy);
+
+        $taxonomy_sql = "
+            (
+                SELECT 
+                    COUNT($taxonomy.term_taxonomy_id) * $term_weight 
+
+                FROM 
+                    $wpdb->term_relationships $taxonomy 
+                WHERE 
+                    $taxonomy.object_id = p.ID AND 
+                    $taxonomy.term_taxonomy_id IN ($terms)
+            )
+        ";
+
+        return $taxonomy_sql;
+    }
+
+    /**
+     * Return the ratings parts of the SQL query
+     *
+     * @param integer $term_id
+     * @return array
+     */
+    public function get_rating_sql($term_id)
+    {
+        $collection_post_type = get_post_type($term_id);
+
+        $config = $this->get_config($collection_post_type);
+
+        $rating_sqls = [];
+
+        if (isset($config['weights'])) {
+
+            foreach ($config['weights'] as $taxonomy => $weight) {
+                if (!$weight) {
+                    continue;
+                }
+
+                $rating_sqls[] = $this->get_taxonomy_rating_sql($term_id, $taxonomy, $weight);
+            }
+        }
+
+        return $rating_sqls;
+    }
+
+
+    /**
+     * Return the related items query params
+     *
+     * @param integer $term_id
+     * @param integer $num
+     * @return array
+     */
+    public function get_items_query_params($term_id = null, $num = 6)
+    {
+        global $wpdb;
+
+        if (is_null($term_id)) {
+            $term_id = get_the_ID();
+        }
+
+        $config = $this->get_config(get_post_type($term_id));
+
+        $collections = $config['collections'];
+
+        $collections_string = implode("','", $collections);
+
+        $rating_sqls = $this->get_rating_sql($term_id);
+        
+        $rating_sql = implode(' + ', $rating_sqls);
+
+        $sql = "
+        SELECT
+            p.ID,
+            (
+                $rating_sql
+            ) AS rating
+
+        FROM $wpdb->posts p
+        WHERE
+            p.post_type IN ('$collections_string') AND
+            p.post_status = 'publish' AND
+            p.ID <> $term_id
+        GROUP BY p.ID
+        ORDER BY rating DESC, RAND()
+        LIMIT $num";
+
+        echo "<pre>$sql</pre>";
+        $result = $wpdb->get_results($sql);
+
+        $ids = array_map(function ($el) {
+            return $el->ID;
+        }, $result);
+
+        if (!$ids) {
+            $ids = [-1];
+        }
+
+        return [
+            'post__in' => $ids,
+            'post_type' => $collections,
+            'posts_per_page' => -1,
+            'orderby' => 'post__in'
+        ];
+    }
+
+    /**
+     * Return the related items WP_Query
+     *
+     * @param integer $term_id
+     * @param integer $num
+     * @return \WP_Query
+     */
+    public function get_items_query($term_id = null, $num = 6)
+    {
+        $params = $this->get_items_query_params($term_id, $num);
+        return new \WP_Query($params);
+    }
+
+    /**
+     * Return the related items
+     *
+     * @param integer $term_id
+     * @param integer $num
+     * @return \WP_Posts[]
+     */
+    public function get_items($term_id = null, $num = 6)
+    {
+        $params = $this->get_items_query_params($term_id, $num);
+        return get_posts($params);
+    }
+}
+
+TainacanRelatedItems::getInstance();
